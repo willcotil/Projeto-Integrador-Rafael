d24db7f583d0e9312fdb6dce963f96110bbab9aa
Rafael <morgothcrownediniron@gmail.com>
2026-03-30 20:45:53 -0300

    UsuarioService feito

---
+++ src/main/java/com/backendpi/backend/service/UsuarioService.java
@@ -0,0 +1,33 @@
+package com.backendpi.backend.service;
+
+import com.backendpi.backend.model.Usuario;
+import com.backendpi.backend.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository repository;
+
+    public UsuarioService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Usuario> listar() {
+        return repository.findAll();
+    }
+
+    public Usuario salvar(Usuario usuario) {
+        return repository.save(usuario);
+    }
+
+    public Usuario buscarPorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
